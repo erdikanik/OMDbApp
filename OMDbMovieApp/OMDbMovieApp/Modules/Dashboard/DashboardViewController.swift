0df4427baef1a9a5a60923c22ad81957d8dcafc9
ka-ni-ke <erdikanik@gmail.com>
2023-05-03 16:28:50 +0300

    Add search controller

--- OMDbMovieApp/OMDbMovieApp/Modules/Dashboard/DashboardViewController.swift
+++ OMDbMovieApp/OMDbMovieApp/Modules/Dashboard/DashboardViewController.swift
@@ -12,9 +12,51 @@ final class DashboardViewController: UIViewController {
 
     var viewModel: DashboardViewModelInterface?
 
+    private let searchController = UISearchController(searchResultsController: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: Will be implemented
+        title = "Movies"
+        applyStyle()
+        applySearchController()
     }
+}
+
+// MARK: Views
+
+private extension DashboardViewController {
+
+    func applyStyle() {
+        view.backgroundColor = .white
+    }
+
+    func applySearchController() {
+
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Movies"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+
+        searchController.searchBar.delegate = self
+    }
+}
+
+// MARK: UISearchResultsUpdating
+
+extension DashboardViewController: UISearchResultsUpdating {
+
+  func updateSearchResults(for searchController: UISearchController) {
+      // TODO: Will be implemented
+  }
+}
+
+// MARK: UISearchBarDelegate
+
+extension DashboardViewController: UISearchBarDelegate {
+
+  func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+      // TODO: Will be implemented
+  }
 }
