8867e6fca1364e76fa9b1edc50a62a7129a8de4a
ka-ni-ke <erdikanik@gmail.com>
2023-05-04 10:26:59 +0300

    Add stateChangeHandler and movie list

--- OMDbMovieApp/OMDbMovieApp/Modules/Dashboard/DashboardViewModel.swift
+++ OMDbMovieApp/OMDbMovieApp/Modules/Dashboard/DashboardViewModel.swift
@@ -7,12 +7,22 @@
 
 import Foundation
 
-protocol DashboardViewModelInterface {
+enum DashboardViewModelState {
 
-    // TODO: Will be implemented
+    case initialMovies([Movie], [Movie])
+    case error(String)
 }
 
-final class DashboardViewModel: DashboardViewModelInterface {
+protocol DashboardViewModelInterface {
+
+    /// State change handler of dashboard view model
+    var stateChangeHandler: ((DashboardViewModelState) -> Void)? { get set }
+
+    /// Fetches initial movies for table view and collection view
+    func fetchInitialMovies()
+}
+
+final class DashboardViewModel {
 
     private enum Constant {
 
@@ -22,11 +32,23 @@ final class DashboardViewModel: DashboardViewModelInterface {
 
     let networkManager: NetworkManagerProtocol
 
+    var stateChangeHandler: ((DashboardViewModelState) -> Void)?
+
     private var currentTableViewPage = 1
     private var currentCollectionViewPage = 1
 
+    private var tableViewMovies: [Movie] = []
+    private var collectionViewMovies: [Movie] = []
+
     init(networkManager: NetworkManagerProtocol) {
         self.networkManager = networkManager
+    }
+}
+
+extension DashboardViewModel: DashboardViewModelInterface {
+
+    func fetchInitialMovies() {
+        callMovieServicesForTableViewAndCollectionView()
     }
 }
 
@@ -38,18 +60,31 @@ private extension DashboardViewModel {
         let group = DispatchGroup()
 
         group.enter()
-        callMovieService(searchKey: Constant.initialTableViewSearchKey, page: currentTableViewPage) { response, error in
+        callMovieService(
+            searchKey: Constant.initialTableViewSearchKey,
+            page: currentTableViewPage
+        ) { [weak self] response, error in
 
+            if let results = response?.searchResults {
+                self?.tableViewMovies.append(contentsOf: results)
+            }
             group.leave()
         }
 
         group.enter()
-        callMovieService(searchKey: Constant.collectionViewDefaultSearchKey, page: currentCollectionViewPage) { response, error in
+        callMovieService(
+            searchKey: Constant.collectionViewDefaultSearchKey,
+            page: currentCollectionViewPage
+        ) { [weak self] response, error in
+
+            if let results = response?.searchResults {
+                self?.collectionViewMovies.append(contentsOf: results)
+            }
             group.leave()
         }
 
         group.notify(queue: .main) {
-            // TODO: Will be implemented
+            self.stateChangeHandler?(.initialMovies(self.tableViewMovies, self.collectionViewMovies))
         }
     }
 
