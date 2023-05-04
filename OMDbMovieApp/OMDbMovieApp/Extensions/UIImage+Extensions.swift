440c93d6f165a73c75af25e03b36d325daf3c22f
ka-ni-ke <erdikanik@gmail.com>
2023-05-04 17:24:02 +0300

    Add UIImage Extension to download images from url

---
+++ OMDbMovieApp/OMDbMovieApp/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  UIImage+Extensions.swift
+//  OMDbMovieApp
+//
+//  Created by Erdi Kanık on 4.05.2023.
+//
+
+import UIKit
+
+extension UIImageView {
+
+    func load(url: String?, imageName: String) {
+        DispatchQueue.main.async {
+            self.backgroundColor = .white
+
+            let imageData = FileUtil.getFile(by: imageName)
+
+            guard imageData == nil else {
+                DispatchQueue.main.async {
+                    self.image = UIImage(data: imageData ?? Data())
+                }
+                return
+            }
+
+            guard let url = url, let imageUrl = URL(string: url) else {
+                return
+            }
+
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: imageUrl) {
+                    if let image = UIImage(data: data) {
+                        FileUtil.saveFile(by: imageName, data: data)
+
+                        DispatchQueue.main.async {
+                            self?.image = image
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
