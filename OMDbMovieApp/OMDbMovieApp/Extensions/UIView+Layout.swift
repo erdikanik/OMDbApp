14650c60d4eee214f1e6c485fc987e3b64c9b77b
ka-ni-ke <erdikanik@gmail.com>
2023-05-04 11:25:35 +0300

    Add cover to superview for safelayout

--- OMDbMovieApp/OMDbMovieApp/Extensions/UIView+Layout.swift
+++ OMDbMovieApp/OMDbMovieApp/Extensions/UIView+Layout.swift
@@ -21,6 +21,18 @@ extension UIView {
             return
         }
 
+        leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: edgeInsents.left).isActive = true
+        trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -edgeInsents.right).isActive = true
+        topAnchor.constraint(equalTo: superview.topAnchor, constant: edgeInsents.top).isActive = true
+        bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: edgeInsents.bottom).isActive = true
+    }
+
+    func coverToSuperViewSafeArea(edgeInsents: UIEdgeInsets =  UIEdgeInsets.zero) {
+
+        guard let superview = superview else {
+            return
+        }
+
         leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor, constant: edgeInsents.left).isActive = true
         trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor, constant: -edgeInsents.right).isActive = true
         topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor, constant: edgeInsents.top).isActive = true
