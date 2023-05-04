53a59cbaaeedc56d7d1a9f7a1b9bbe718e1f0e4c
ka-ni-ke <erdikanik@gmail.com>
2023-05-05 01:25:12 +0300

    Add movie collection cell

--- OMDbMovieApp/OMDbMovieApp/Views/Cells/MovieCollectionViewCell.swift
+++ OMDbMovieApp/OMDbMovieApp/Views/Cells/MovieCollectionViewCell.swift
@@ -5,4 +5,15 @@
 //  Created by Erdi Kanık on 5.05.2023.
 //
 
-import Foundation
+import UIKit
+
+final class MovieCollectionCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
